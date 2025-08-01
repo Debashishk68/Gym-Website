98b68b6511fcfc5b7c1b1c99f92c5bb149a8c463
debashishk69 <debashishkarmakar326@gmail.com>
2025-08-01 13:27:39 +0530

    added vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,11 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    historyApiFallback: true, // <== allows SPA routing
-  },
   resolve: {
-  extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
-}
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
+  }
 });
