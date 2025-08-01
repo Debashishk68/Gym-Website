2652070393e63bed1fa3efdd5c8aced21d5cca78
debashishk69 <debashishkarmakar326@gmail.com>
2025-08-01 12:35:59 +0530

    added vercel

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: "http://localhost:5173", // Frontend URL
+  origin: process.env.FRONTEND_URL, // Frontend URL
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
